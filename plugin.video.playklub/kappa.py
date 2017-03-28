33c12e15fd2d0fb2f3625bdf1542b016ace5dfae
Trevor Driscoll <ubpd34th@gmail.com>
2017-03-28 00:25:28 -0400

    Decode kappa.py

--- plugin.video.playklub/kappa.py
+++ plugin.video.playklub/kappa.py
@@ -5,12 +5,12 @@ import xbmcgui
 def change_code():
 	xbmc.executebuiltin('Dialog.Close(10140)')
 	dialog = xbmcgui.Dialog()
-	currentcode = plugintools.get_setting(sync_data("dmFuZW1ha29vZA=="))
-	currentcodeinput = dialog.input(sync_data('UGxlYXNlIGVudGVyIHlvdXIgY3VycmVudCBQYXJlbnRhbCBDb2Rl'), type=xbmcgui.INPUT_NUMERIC, option=xbmcgui.ALPHANUM_HIDE_INPUT)
+	currentcode = plugintools.get_setting("vanemakood")
+	currentcodeinput = dialog.input("Please enter your current Parental Code", type=xbmcgui.INPUT_NUMERIC, option=xbmcgui.ALPHANUM_HIDE_INPUT)
 	if currentcodeinput == currentcode or currentcode == '':
-		newcodeinput = dialog.input(sync_data('UGxlYXNlIHNldCB5b3VyIG5ldyBQYXJlbnRhbCBDb2Rl'), type=xbmcgui.INPUT_NUMERIC, option=xbmcgui.ALPHANUM_HIDE_INPUT)
-		plugintools.set_setting(sync_data("dmFuZW1ha29vZA=="), newcodeinput)
-		plugintools.set_setting(sync_data("dmFuZW1hbHVraw=="), 'true')
+		newcodeinput = dialog.input("Please set your new Parental Code", type=xbmcgui.INPUT_NUMERIC, option=xbmcgui.ALPHANUM_HIDE_INPUT)
+		plugintools.set_setting("vanemakood", newcodeinput)
+		plugintools.set_setting("vanemalukk", 'true')
 		plugintools.open_settings_dialog()
 	else:
 		xbmc.executebuiltin((u'XBMC.Notification("Parental-Lock Error!", "Incorrect code!", 3000)'))
